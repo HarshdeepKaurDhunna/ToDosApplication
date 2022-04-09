2022bb8adae1c412f215eeac915d0fd39d10b387
HarshdeepKaurDhunna <97932988+HarshdeepKaurDhunna@users.noreply.github.com>
2022-04-08 22:36:45 -0400

    Add Edit and Delete Page

--- ToDosApplication/Pages/Create.cshtml.cs
+++ ToDosApplication/Pages/Create.cshtml.cs
@@ -3,23 +3,21 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using ToDosApplication.Models;
 using System.Threading.Tasks;
 using System.Diagnostics;
-namespace ToDosApplication.Pages
-{
+namespace ToDosApplication.Pages { 
 
-    public class CreateModel : PageModel
+public class CreateModel : PageModel
+{
+    [BindProperty]
+    public ToDo ToDo { get; set; }
+    public void OnGet()
     {
-        [BindProperty]
-        public ToDo ToDo { get; set; }
-        public void OnGet()
-        {
-        }
-        public IActionResult OnPost()
-        {
-            IndexModel IndexModel = new IndexModel();
-            ToDo.AddDate = System.DateTime.Now;
-            ToDo.ID = 4;
-            IndexModel.Add(ToDo);
-            return RedirectToPage("./Index");
-        }
+    }
+    public IActionResult OnPost()
+    {
+        IndexModel IndexModel = new IndexModel();
+        ToDo.AddDate = System.DateTime.Now;
+        IndexModel.Add(ToDo);
+        return RedirectToPage("./Index");
     }
 }
+    }
